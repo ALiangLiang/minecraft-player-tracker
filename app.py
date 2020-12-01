e960e80502f0a97e2f45825e308fa392eaad056f
ALiangLiang <me@aliangliang.top>
2020-12-01 20:02:34 +0000

    first mvp

---
+++ app.py
@@ -0,0 +1,43 @@
+#!.venv/bin/python3
+from mcstatus import MinecraftServer
+import os
+from sqlalchemy import create_engine
+from sqlalchemy import Column, Integer, String, DateTime, func, ForeignKey
+from sqlalchemy.orm import relationship, Session
+from sqlalchemy.ext.declarative import declarative_base
+
+# declarative base class
+Base = declarative_base()
+
+class Operation(Base):
+    __tablename__ = 'operation'
+
+    id = Column(Integer, primary_key=True)
+    count = Column(Integer)
+    created_at = Column(DateTime, nullable=False, server_default=func.now())
+    players = relationship('Player')
+
+class Player(Base):
+    __tablename__ = 'player'
+
+    id = Column(Integer, primary_key=True)
+    name = Column(String, nullable=False)
+    operation_id = Column(Integer, ForeignKey('operation.id'))
+
+engine = create_engine(os.environ['DATABASE_URL'], echo=True, future=True)
+
+# If you know the host and port, you may skip this and use MinecraftServer("example.org", 1234)
+server = MinecraftServer.lookup(os.environ['MINECRAFT_SERVER_HOST'])
+
+# 'status' is supported by all Minecraft servers that are version 1.7 or higher.
+status = server.status()
+print(status.players.online)
+print([sample.name for sample in status.players.sample])
+
+with Session(engine) as session:
+  operation = Operation(count=status.players.online)
+  players = [Player(name=sample.name) for sample in status.players.sample]
+  operation.players = players
+  session.add(operation)
+  session.add_all(players)
+  session.commit()
